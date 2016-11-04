e1f80f0bd17d270f73f052ef2d6fbb3f28c7188c
jankotek <jankotek@atlas.cz>
2016-11-04 23:18:45 +0100

    S5L61 - Constructor Functions & Arguments initial: constructor with arguments console: Object { name: "Max", age: 30 }script.js:7:1

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-function Person() {
-    this.name = 'Max';
-    this.greet = function () {
-        console.log('Hello from within constructor, ' + this.name + ' ' + this.surname);
-    };
+function  Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
-Person.prototype.greet2 = function () {
-    console.log('Hello ' + this.name + ' ' + this.surname);
-};
-
-var person = new Person();
-person.name = 'Jan';
-
-Person.prototype.name = 'WontApply'; /* prototype is one level higher. Does not override constructor fields*/
-Person.prototype.surname = 'Kotek';
-
-
-var max = new Person();
-
-person.greet();
-person.greet2();
-max.greet();
-max.greet2();
-
-console.log(person instanceof Person);
-
-var personPrototype = Object.getPrototypeOf(Person);
-console.log(personPrototype instanceof Person);
-console.log(personPrototype == Person.__proto__);
+var maxPerson = new Person('Max', 30);
+console.log(maxPerson);
