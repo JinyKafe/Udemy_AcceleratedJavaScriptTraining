fb0604b105687fa073f4f4444a05ee3b1f69ec79
jankotek <jankotek@atlas.cz>
2016-11-04 23:39:34 +0100

    S5L61 - Constructor Functions & Arguments !!! 4 ways how to create object !!! console: Object { name: "Max" }script.js:4:1 truescript.js:5:1 Object { name: "Max" }script.js:10:1 truescript.js:11:1 Object { name: "Max" }script.js:16:1 falsescript.js:17:1 Object {  }script.js:21:1 truescript.js:22:1 Object { name: "Max" }script.js:29:1 truescript.js:30:1 truescript.js:31:1

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-function  Person(name, age) {
-    this.name = name;
-    this.age = age;
-}
+/*There are 4 ways to create object*/
+/*1. create Object instance and set new variables*/
+var person1 = {name: 'Max'};
+console.log(person1);
+console.log(person1 instanceof Object);
 
-var maxPerson = new Person('Max', 30);
-console.log(maxPerson);
-var annaPerson = new Person('Anna');
-console.log(annaPerson);
+/*2. the same like above (but more messy). Create Object instance and set new variables*/
+var person2 = new Object();
+person2.name = 'Max';
+console.log(person2);
+console.log(person2 instanceof Object);
+
+/*3. create instance from prototype (can be also null )*/
+var person3 = Object.create(null);
+person3.name = 'Max';
+console.log(person3);
+console.log(person3 instanceof Object);
+//
+var person4 = Object.create(Object);
+person4.name = 'Max';
+console.log(person4);
+console.log(person4 instanceof Object);
+
+/*4. using constructor function*/
+function Person () {
+    this.name = 'Max';
+}
+var person5 = new Person();
+console.log(person5);
+console.log(person5 instanceof Object);
+console.log(person5 instanceof Person);
