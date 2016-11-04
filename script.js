f6829a857fd5b4a7bb4c998e4030d40c596ed9db
jankotek <jankotek@atlas.cz>
2016-11-05 00:47:11 +0100

    S5L65 - Creating Properties with defineProperty() defining one of the function outside the object by 'defineProperty()' console: Account name: Defaultscript.js:14:1 Account name: new Account Namescript.js:16:1 Deposed 2000. Remains 14000script.js:10:9 Withdrew 3000. Remains 11000script.js:6:9

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ var account = {
         this.cash -= amount;
         console.log('Withdrew ' + amount + '. Remains ' + this.cash);
     },
-    depose: function (amount) {
+    /*    depose: function (amount) {
+     this.cash += amount;
+     console.log('Deposed ' + amount + '. Remains ' + this.cash);
+     }*/
+};
+
+Object.defineProperty(account, 'depose', {
+    value: function (amount) {
         this.cash += amount;
         console.log('Deposed ' + amount + '. Remains ' + this.cash);
     }
-};
-
+});
 console.log('Account name: ' + account._name);
 account._name = 'new Account Name';
 console.log('Account name: ' + account._name);
