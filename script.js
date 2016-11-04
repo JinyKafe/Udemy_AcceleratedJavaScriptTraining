3071703ffddbfc2eabc2bcdd24b9df64fdbfa110
jankotek <jankotek@atlas.cz>
2016-11-04 23:04:18 +0100

    S5L60 - Constructor Functions - prototype is one level higher than constructor and defining field or function via prototype does not override constructor field or funktions. To use prototype field or funktions, they have to have different names console: Hello from within constructor, Jan Kotekscript.js:4:9 Hello Jan Kotekscript.js:9:5 Hello from within constructor, Max Kotekscript.js:4:9 Hello Max Kotekscript.js:9:5

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function Person() {
     this.name = 'Max';
     this.greet = function () {
-        console.log('Hello from within constructor, ' + this.name);
+        console.log('Hello from within constructor, ' + this.name + ' ' + this.surname);
     };
 }
 
 Person.prototype.greet2 = function () {
-    console.log('Hello ' + this.name);
+    console.log('Hello ' + this.name + ' ' + this.surname);
 };
 
 var person = new Person();
 person.name = 'Jan';
+
+Person.prototype.name = 'WontApply'; /* prototype is one level higher. Does not override constructor fields*/
+Person.prototype.surname = 'Kotek';
 
 var max = new Person();
 
