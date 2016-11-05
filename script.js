83f7c3c437dd069eecdb00cf67f06d417409df32
jankotek <jankotek@atlas.cz>
2016-11-05 01:01:52 +0100

    S5L65 - Creating Properties with defineProperty() defining getters and setters via defineProperty() console: Account _name directly: Defaultscript.js:29:1 Account _name directly after change: new Namescript.js:31:1 Account name via getter: fakedNamescript.js:32:1 Account name: new Account Namescript.js:34:1 Deposed 2000. Remains 14000script.js:17:9 Withdrew 3000. Remains 11000script.js:6:9

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var account = {
     cash: 12000,
+    _name: 'Default',
     withdraw: function (amount) {
         this.cash -= amount;
         console.log('Withdrew ' + amount + '. Remains ' + this.cash);
@@ -17,12 +18,18 @@ Object.defineProperty(account, 'depose', {
     }
 });
 
-Object.defineProperty(account, '_name', {
-    value: 'Default',
-    //writable: false // default value
-    writable:true
+Object.defineProperty(account, 'name', {
+    get: function () {
+        return 'fakedName';
+    },
+    set: function (name) {
+        this._name = name
+    }
 });
-console.log('Account name: ' + account._name);
+console.log('Account _name directly: ' + account._name); // direct access
+account.name = 'new Name';
+console.log('Account _name directly after change: ' + account._name); // direct access
+console.log('Account name via getter: ' + account.name); // using getter
 account._name = 'new Account Name'; // we can override the variable since writable:true
 console.log('Account name: ' + account._name);
 //
