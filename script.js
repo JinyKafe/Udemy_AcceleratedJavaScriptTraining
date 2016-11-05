e02e26277f4b36fcea870b2973b99ee9e4a2ab49
jankotek <jankotek@atlas.cz>
2016-11-05 01:13:16 +0100

    S5L66 - special functions for manipulating objects console: Object { name: "Max", age: 27, greet: person.greet() }script.js:9:1 Object { age: 27, greet: person.greet() }script.js:11:1 truescript.js:14:1 agescript.js:18:5 greetscript.js:18:5

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-var account = {
-    cash: 12000,
-    _name: 'Default',
-    withdraw: function (amount) {
-        this.cash -= amount;
-        console.log('Withdrew ' + amount + '. Remains ' + this.cash);
-    },
-    /*    depose: function (amount) {
-     this.cash += amount;
-     console.log('Deposed ' + amount + '. Remains ' + this.cash);
-     }*/
+var person = {
+    name: 'Max',
+    age: 27,
+    greet: function () {
+        console.log('Hello');
+    }
 };
 
-Object.defineProperty(account, 'depose', {
-    value: function (amount) {
-        this.cash += amount;
-        console.log('Deposed ' + amount + '. Remains ' + this.cash);
-    }
-});
-
-Object.defineProperty(account, 'name', {
-    get: function () {
-        return 'fakedName';
-    },
-    set: function (name) {
-        this._name = name
-    }
-});
-console.log('Account _name directly: ' + account._name); // direct access
-account.name = 'new Name';
-console.log('Account _name directly after change: ' + account._name); // direct access
-console.log('Account name via getter: ' + account.name); // using getter
-account._name = 'new Account Name'; // we can override the variable since writable:true
-console.log('Account name: ' + account._name);
+console.log(person);
+delete person.name; // delete variable
+console.log(person);
 //
-account.depose(2000);
-account.withdraw(3000);
+// check presence of variable
+console.log('age' in person);
+//
+// looping all person's fields
+for (var obj in person) {
+    console.log(obj);
+}
